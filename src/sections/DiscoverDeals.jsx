cea3fcee70d4dbbe27feff2c3863546b99523747
Mehedi Hasan <mehediyyzz@gmail.com>
2024-12-20 21:53:37 +0600

    added carousel

--- src/sections/DiscoverDeals.jsx
+++ src/sections/DiscoverDeals.jsx
@@ -1,23 +1,54 @@
 import PropertyCard from "../components/PropertyCard";
 import { propertyData } from "../constants";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
 const DiscoverDeals = () => {
   return (
     <section className="container mx-auto padding-b">
       <h2 className="h2 text-center px-2">Discover Our Best Deals</h2>
       <p className="text-center">Lorem ipsum dolor sit amet</p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
-        {propertyData.slice(0, 3).map((property) => (
-          <PropertyCard
-            key={property.id}
-            badge={property.badge}
-            imgUrl={property.imgUrl}
-            title={property.title}
-            address={property.address}
-            count={property.count}
-            price={property.price}
-          />
-        ))}
+      <div className="mt-6">
+        <Swiper
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {propertyData.slice(0, 5).map((property) => (
+            <SwiperSlide key={property.id}>
+              <PropertyCard
+                key={property.id}
+                badge={property.badge}
+                imgUrl={property.imgUrl}
+                title={property.title}
+                address={property.address}
+                count={property.count}
+                price={property.price}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
--- src/sections/RecentProperties.jsx
+++ src/sections/RecentProperties.jsx
@@ -1,5 +1,13 @@
 import PropertyCard from "../components/PropertyCard";
 import { propertyData } from "../constants";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 const RecentProperties = () => {
   return (
@@ -7,7 +15,7 @@ const RecentProperties = () => {
       <section className="container mx-auto padding-t padding-b">
         <h2 className="h2 text-center px-2">Recent Properties for Rent</h2>
         <p className="text-center py-4">Lorem ipsum dolor sit amet</p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-6">
+        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-6">
           {propertyData.slice(0, 4).map((property) => (
             <PropertyCard
               key={property.id}
@@ -19,24 +27,88 @@ const RecentProperties = () => {
               price={property.price}
             />
           ))}
+        </div> */}
+        <div className="mt-6 px-2">
+          <Swiper
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+              1280: {
+                slidesPerView: 4,
+              },
+            }}
+          >
+            {propertyData.slice(4, 9).map((property) => (
+              <SwiperSlide key={property.id}>
+                <PropertyCard
+                  key={property.id}
+                  badge={property.badge}
+                  imgUrl={property.imgUrl}
+                  title={property.title}
+                  address={property.address}
+                  count={property.count}
+                  price={property.price}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </section>
 
       <section className="container mx-auto padding-b">
         <h2 className="h2 text-center px-2">Recent Properties for Sale</h2>
         <p className="text-center py-4">Lorem ipsum dolor sit amet</p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-6">
-          {propertyData.slice(5, 9).map((property) => (
-            <PropertyCard
-              key={property.id}
-              badge={property.badge}
-              imgUrl={property.imgUrl}
-              title={property.title}
-              address={property.address}
-              count={property.count}
-              price={property.price}
-            />
-          ))}
+
+        <div className="mt-6 px-2">
+          <Swiper
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+              1280: {
+                slidesPerView: 4,
+              },
+            }}
+          >
+            {propertyData.slice(0, 6).map((property) => (
+              <SwiperSlide key={property.id}>
+                <PropertyCard
+                  key={property.id}
+                  badge={property.badge}
+                  imgUrl={property.imgUrl}
+                  title={property.title}
+                  address={property.address}
+                  count={property.count}
+                  price={property.price}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </section>
     </>
